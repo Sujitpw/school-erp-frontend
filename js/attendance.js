cc61b275c22ce3fffa6d162df0c29f2e50edc7ae
Sujit Kumar <thesujit9155@gmail.com>
2026-08-04 14:44:34 +0530

    Fix add attendance page link

--- js/attendance.js
+++ js/attendance.js
@@ -132,7 +132,7 @@ async function deleteAttendance(id) {
 function editAttendance(id) {
 
     window.location.href =
-        "add-attendance_page.html?id=" + id;
+        "attendance_page.html?id=" + id;
 
 }
 
